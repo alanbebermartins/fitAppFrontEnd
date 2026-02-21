2d5d991d834ddcc2589a821ff2fc51b8e204278a
Alan Beber Martins <abebermartins@gmail.com>
2026-02-21 18:09:39 -0300

    Changeg the dashboard code to avoid local cache, and not allow access to the dashboard via the url

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,22 +1,32 @@
-document.addEventListener("DOMContentLoaded", async () => {
+window.addEventListener("pageshow", function (event) {
+    if (event.persisted) {
+        window.location.reload();
+    }
+});
 
+document.addEventListener("DOMContentLoaded", async () => {
     try {
+
         const response = await fetch("http://127.0.0.1:8000/api/auth/me/", {
             method: "GET",
             credentials: "include"
         });
 
+        if (response.status === 401) {
+            window.location.replace("/index.html");
+            return;
+        }
+
         if (!response.ok) {
-            window.location.href = "/index.html";
+            console.error("Erro inesperado:", response.status);
             return;
         }
 
         const user = await response.json();
-        console.log("Usuário logado:", user);
+        console.log("Usuário validado:", user);
 
     } catch (error) {
-        console.error("Erro ao validar sessão:", error);
-        window.location.href = "/index.html";
+        console.error("Erro de conexão:", error);
+        window.location.replace("/index.html");
     }
-
 });
