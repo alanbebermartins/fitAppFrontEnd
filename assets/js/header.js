ca80ec50d33242d8d05870deb3bbc009b4646baa
Alan Beber Martins <abebermartins@gmail.com>
2026-09-13 20:33:26 -0300

    Adicionado prevent default

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,12 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const dashboardButton = document.getElementById('dashboardButton')
     const graphButton = document.getElementById('graphButton')
 
-    dashboardButton.addEventListener('click', () => {
+    dashboardButton.addEventListener('click', (event) => {
+        event.preventDefault()
         console.log('botao dashboard clicado')
         window.location.replace('/home') 
     })
     
-    graphButton.addEventListener('click', () => {
+    graphButton.addEventListener('click', (event) => {
+        event.preventDefault()
         console.log('botao do grafico clicado')
         window.location.replace('/grafico-de-progresso') 
     })
